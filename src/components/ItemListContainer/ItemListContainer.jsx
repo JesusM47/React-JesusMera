b0432c9eb83d79fdb1fecdd9d4ecf289443efe3b
JesusM47 <jesus_mf_0114@hotmail.com>
2025-09-03 06:58:04 -0600

    test

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
-
-import { getLibros, getLibrosCategoria } from "../../asyncapi"
+// import { getLibros, getLibrosCategoria } from "../../asyncapi"
 import { ItemList } from "../ItemList/ItemList"
 import { useParams } from "react-router-dom"
-
+import { db } from "../../services/config"
+import { collection, getDocs, query, where} from "firebase/firestore"
 
 
  const ItemListContainer = () => {
@@ -12,19 +12,54 @@ import { useParams } from "react-router-dom"
 
   const {idCategoria} = useParams()
 
-  useEffect(() => {
+useEffect(() => {
+  const misLibros = idCategoria 
+    ? query(collection(db, "libros"), where("idCat", "==", idCategoria))
+    : collection(db, "libros")
 
-    const funcionLibros = idCategoria ? getLibrosCategoria : getLibros
+  getDocs(misLibros)
+    .then(res => {
+      const nuevosLibros = res.docs.map(doc => {
+        const data = doc.data()
 
-    funcionLibros(idCategoria)
-      .then(res =>  setLibros(res))
+        return {id: doc.id, ...doc.data()}
 
-    // getLibros()
-    // .then (respusta => setLibros(respusta))
-    // .catch(error => console.log(error))
-  },[idCategoria])
+        //   return{
+        // //   id: doc.id,
+        // //   ...doc.data()
+        //   }
+        }
+      })
+      setLibros(nuevosLibros)
+    })
+    .catch(error => console.log(error))
 
-  console.log(libros)
+})
+
+
+  // useEffect(() => {
+
+  //   const funcionLibros = idCategoria ? getLibrosCategoria : getLibros
+
+
+  //   funcionLibros(idCategoria)
+  //     .then(res =>  setLibros(res))
+
+  //   // getLibros()
+  //   // .then (respusta => setLibros(respusta))
+  //   // .catch(error => console.log(error))
+  // },[idCategoria]
+
+  //   funcionLibros(idCategoria)
+  //     .then(res => () {})
+
+  //   // getLibros()
+  //   // .then (respusta => setLibros(respusta))
+  //   // .catch(error => console.log(error))
+  // },[])
+
+
+  // console.log(libros)
 
   
 
@@ -37,8 +72,14 @@ import { useParams } from "react-router-dom"
      
       
      </>
+  // return (
+    
+  //    <div>
+  //     <h2 style={{ textAlign: "center"}}>Mis Libros</h2>
+  //     <ItemList libros={libros} />
+  //    </div>
 
-  )
-}
+  // )
+)}
 
 export default ItemListContainer
